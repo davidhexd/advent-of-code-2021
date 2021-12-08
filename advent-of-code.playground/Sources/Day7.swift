d9cc4750a777b3ba7dd5ba65532492aee45ca794
David He <davidhe@makenotion.com>
2021-12-07 20:50:54 -0800

    Day 7 part 2

--- advent-of-code.playground/Sources/Day7.swift
+++ advent-of-code.playground/Sources/Day7.swift
@@ -2,13 +2,13 @@ import Foundation
 
 public let day7 = Day(name: "seven") {
   part1()
+  part2()
 }
 
 private func part1() {
   let crabPositions = parseInput(fileName: "day-7")
   let sorted = crabPositions.sorted()
     
-  // Do binary search?
   let min = sorted.first!
   let max = sorted.last!
 
@@ -23,6 +23,31 @@ private func part1() {
   }
   
   print("Part 1: \(bruteForce.first)")
+}
+
+private func part2() {
+  let crabPositions = parseInput(fileName: "day-7")
+  let sorted = crabPositions.sorted()
+  
+  let min = sorted.first!
+  let max = sorted.last!
+
+  func costToTravel(currentCrabPosition: Int, desiredPosition: Int) -> Int {
+    let distance = abs(currentCrabPosition - desiredPosition)
+    return distance * (distance + 1) / 2
+  }
+  
+  let bruteForce = (min...max).map { position -> (Int, Int) in
+    let travelCost = crabPositions.reduce(0) { totalCost, currentCrabPosition in
+      return totalCost + costToTravel(currentCrabPosition: currentCrabPosition, desiredPosition: position)
+    }
+
+    return (position, travelCost)
+  }.sorted { a, b in
+    return a.1 < b.1
+  }
+  
+  print("Part 2: \(bruteForce.first)")
 }
 
 private func parseInput(fileName: String) -> [Int] {
