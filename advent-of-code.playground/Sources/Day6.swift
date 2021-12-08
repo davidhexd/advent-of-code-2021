a6607f4e03824d0ce7889cbbb7d1289700f94e6a
David He <davidhe@makenotion.com>
2021-12-07 20:26:07 -0800

    Day 6 part 2

--- advent-of-code.playground/Sources/Day6.swift
+++ advent-of-code.playground/Sources/Day6.swift
@@ -6,23 +6,21 @@ public let day6 = Day(name: "six") {
 }
 
 private func part1() {
-//  print("Part 1: \(modelFish_slow(fileName: "day-6", after: 80))")
+  print("Part 1: \(modelFish_slow(initialFish: parseInput(fileName: "day-6"), after: 80))")
 }
+
 private func part2() {
-  print("Part 2")
-  let initialFish = parseInput(fileName: "day-6-example")
-  print((1...7).map { "\($0):\(modelFish_fast(initialFish: [$0], after: 18))"}.joined(separator: "\n"))
-  
-  let s_1 = Date()
-  print("fast: \(modelFish_fast(initialFish: initialFish, after: 18))")
-  print(Date().timeIntervalSince1970 - s_1.timeIntervalSince1970)
-  let s_0 = Date()
-  print("slow: \(modelFish_slow(initialFish: initialFish, after: 18))")
-  print(Date().timeIntervalSince1970 - s_0.timeIntervalSince1970)
+  print("Part 2: \(modelFish_fast(initialFish: parseInput(fileName: "day-6"), after: 256))")
 }
+
+let debug = false
 
 
 private func modelFish_slow(initialFish: [Int], after days: Int) -> Int {
+  if debug {
+    print("\(days): \(initialFish)")
+  }
+  
   let finalFish = (1...days).reduce(initialFish) { currentFish, day in
     var fishToAdd: [Int] = []
     
@@ -38,25 +36,98 @@ private func modelFish_slow(initialFish: [Int], after days: Int) -> Int {
       }
     
     newFish.append(contentsOf: fishToAdd)
-    print("\(day): \(newFish)")
+    if debug {
+      print("\(days - day): \(newFish)")
+    }
     return newFish
   }
   return finalFish.count
 }
 
 private func modelFish_fast(initialFish: [Int], after days: Int) -> Int {
-  func countFish(current: Int, days: Int, depth: Int) -> Int {
-    if (current >= days) {
-      return 1
+  struct Request: Hashable, CustomStringConvertible {
+    let current: Int
+    let daysRemaining: Int
+    
+    func hash(into hasher: inout Hasher) {
+      hasher.combine(current)
+      hasher.combine(daysRemaining)
     }
     
-    return
-      countFish(current: 6, days: days - current - 1, depth: depth) +
-      countFish(current: 8, days: days - current - 1, depth: depth + 1)
+    var description: String {
+      return "🐟 \(current): \(daysRemaining) days"
+    }
+  }
+  
+  var requests: [Request: Int] = [:]
+  
+  func coundChildren(current: Int, days: Int, depth: Int) -> Int {
+    let request = Request(current: current, daysRemaining: days)
+    if (current >= days) {
+      if debug {
+        print("\(depth.padding())\(request) -> 0")
+      }
+      return 0
+    }
+    
+    if let fish = requests[request] {
+      if debug {
+        print("\(depth.padding())\(request) -> 0 (cached)")
+      }
+      return fish
+    }
+    
+    let numChildren: Int = 1 + Int(floorf(Float(days - current) / Float(7))) + {
+      if (days - current) % 7 == 0 {
+        // If hit zero on the last day, subtract one since child spawns on next day
+        return -1
+      } else {
+        return 0
+      }
+    }()
+                                   
+    let requestFish = numChildren
+    
+    var additionalReqs = [Request]()
+    var shouldContinue = true
+    var currentDays = days - current - 1
+    while (shouldContinue) {
+      if (currentDays < 0) {
+        shouldContinue = false
+      } else {
+        additionalReqs.append(Request(current: 8, daysRemaining: currentDays))
+        currentDays = currentDays - 7
+      }
+    }
+    
+    if debug {
+      print("\(depth.padding())\(request) with \(numChildren) makes \(additionalReqs.count) more requests \(additionalReqs)")
+    }
+    
+    var totalFish = requestFish
+
+    additionalReqs.reversed().forEach { request in
+      let numChildrenOfChild = coundChildren(current: request.current, days: request.daysRemaining, depth: depth + 1)
+      totalFish += numChildrenOfChild
+    }
+    
+    if debug {
+      print("🐟 \(current): \(days) days -> \(totalFish) - calculated")
+    }
+    
+    requests[request] = totalFish
+
+    return totalFish
   }
 
   return initialFish.reduce(0) { fishCount, fish in
-    return fishCount + countFish(current: fish, days: days, depth: 0)
+    return fishCount + coundChildren(current: fish, days: days, depth: 0) + 1
+  }
+}
+
+extension Int {
+  func padding() -> String {
+    (0..<self).map { _ in " " }.joined(separator: "")
   }
 }
 
